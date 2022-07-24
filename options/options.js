97f60210f24a30791d07898ba54dbf55e107580d
Crowdbotics <team@crowdbotics.com>
2022-07-24 23:42:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "pande_outdoor_activ_36390",
+  url: "https://pande_outdoor_activ_36390.botics.co",
+  api: "https://pande_outdoor_activ_36390.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
